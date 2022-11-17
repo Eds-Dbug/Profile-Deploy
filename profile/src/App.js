bbf0af3562ff5b7f91e3edaaa4bcb9faf80598b9
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-11-17 18:27:59 -0500

    renamed the picture to ME once again

--- profile/src/App.js
+++ profile/src/App.js
@@ -3,7 +3,7 @@ import './styles.scss';
 
 import { CertificateData } from './components/grids/CertificateData';
 import { ProjectData } from './components/grids/ProjectData';
-import { Bay, Niagra, me} from './Profile-Pics';
+import { Bay, Niagra, ME} from './Profile-Pics';
 import {
 	BikeCarousel,
 	NavBar,
@@ -20,7 +20,7 @@ export default function App() {
 			<section className="about" id="about">
 				<h1>Home</h1>
 				<div className="pic-of-me d-inline-flex bg-secondary" id="pic-of-me">
-					<img src={me} class="img-fluid" alt="me" width="300" height="500" />
+					<img src={ME} class="img-fluid" alt="me" width="300" height="500" />
 				</div>
 				<div className="about-me d-inline-flex justify-content-center">
 					<TxtMap id="about" />
--- profile/src/Profile-Pics/index.js
+++ profile/src/Profile-Pics/index.js
@@ -13,7 +13,7 @@ import Random_Quote_Machine from './Random_Quote_Machine.png';
 import Survey_Form from './Survey_Form.png';
 import Personal_Portfolio_Eddies_Music from "./Personal_Portfolio_Eddie's_Music.png";
 import In_Progress from './In_Progress.jpg';
-import me from './Me/me.jpg';
+import ME from './Me/me.jpg';
 import Videos_Search_App from './Videos_Search_App.png';
 import Student_List_App from './Student_List_App.png';
 
@@ -21,7 +21,7 @@ export {
 	Bay,
 	Niagra,
 	meImg,
-	me,
+	ME,
 	moustache,
 	giant,
 	JS_Algorithms_Data_Structures_Cert,
