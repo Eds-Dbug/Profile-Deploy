359f5eaacd4e5e63948616da0eeb2bfdc391bb5f
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-11-17 19:24:10 -0500

    revert txtmap

--- profile/src/components/text/TxtMap.js
+++ profile/src/components/text/TxtMap.js
@@ -4,15 +4,13 @@ import '../../index.css';
 
 const TxtMap = (props) => {
 	var r = Text.map((item) => {
-		return (item.id === props.id) ?
-			(<div className="textBlock d-inline-flex border border-2 border-opaque-border rounded m-4">
-				{item.text}
-			</div>):
-			(<div className="textBlock d-inline-flex border border-2 border-opaque-border rounded m-4">
-				nothing to say
-			</div>)
-			
-		
+		if (item.id === props.id) {
+			return (
+				<div className="textBlock d-inline-flex border border-2 border-opaque-border rounded m-4">
+					{item.text}
+				</div>
+			);
+		}
 	});
 	return r;
 };
